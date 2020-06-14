b7437f7ef03a91b8183ed5c838f3bbcd6601c72c
Rakesh Kumar Suthar <rksuthar19@gmail.com>
2020-06-15 02:47:34 +0530

    Removing Sysout from testcases added for detailed analysis

--- tests/com/rks/game/TestChessGameComponents.java
+++ tests/com/rks/game/TestChessGameComponents.java
@@ -28,7 +28,7 @@ public class TestChessGameComponents {
         assertEquals(0, coordinate.getX_pos());
         assertEquals(0, coordinate.getY_pos());
 
-        System.out.println(coordinate);
+        //System.out.println(coordinate);
 
         coordinate = new Coordinate(0, 1);
         assertEquals("A7", coordinate.getIdentifier());
@@ -37,7 +37,7 @@ public class TestChessGameComponents {
         assertEquals(0, coordinate.getX_pos());
         assertEquals(0, coordinate.getY_pos());
 
-        System.out.println(coordinate);
+        //System.out.println(coordinate);
 
         coordinate = new Coordinate(0, 2);
         assertEquals("A6", coordinate.getIdentifier());
@@ -46,7 +46,7 @@ public class TestChessGameComponents {
         assertEquals(0, coordinate.getX_pos());
         assertEquals(0, coordinate.getY_pos());
 
-        System.out.println(coordinate);
+        //System.out.println(coordinate);
 
         coordinate = new Coordinate(0, 7);
         assertEquals("A1", coordinate.getIdentifier());
@@ -55,7 +55,7 @@ public class TestChessGameComponents {
         assertEquals(0, coordinate.getX_pos());
         assertEquals(7, coordinate.getY_pos());
 
-        System.out.println(coordinate);
+        //System.out.println(coordinate);
 
         coordinate = new Coordinate(2, 7);
         assertEquals("C1", coordinate.getIdentifier());
@@ -64,7 +64,7 @@ public class TestChessGameComponents {
         assertEquals(2, coordinate.getX_pos());
         assertEquals(7, coordinate.getY_pos());
 
-        System.out.println(coordinate);
+        //System.out.println(coordinate);
 
         coordinate = new Coordinate(7, 7);
         assertEquals("H1", coordinate.getIdentifier());
@@ -73,7 +73,7 @@ public class TestChessGameComponents {
         assertEquals(7, coordinate.getX_pos());
         assertEquals(7, coordinate.getY_pos());
 
-        System.out.println(coordinate);
+        //System.out.println(coordinate);
 
         coordinate = new Coordinate(2, 3);
         assertEquals("C5", coordinate.getIdentifier());
@@ -82,7 +82,7 @@ public class TestChessGameComponents {
         assertEquals(2, coordinate.getX_pos());
         assertEquals(3, coordinate.getY_pos());
 
-        System.out.println(coordinate);
+        //System.out.println(coordinate);
 
         coordinate = new Coordinate(5, 5);
         assertEquals("F3", coordinate.getIdentifier());
@@ -91,13 +91,13 @@ public class TestChessGameComponents {
         assertEquals(5, coordinate.getX_pos());
         assertEquals(5, coordinate.getY_pos());
 
-        System.out.println(coordinate);
+        //System.out.println(coordinate);
     }
 
     @Test
     public void testChessBoard() {
         ChessBoard chessBoard = new ChessBoard();
-        System.out.println(chessBoard);
+        //System.out.println(chessBoard);
         assertFalse(isValidChessBoardCoordinate(new Coordinate(8, 9)));
         assertFalse(isValidChessBoardCoordinate(new Coordinate(-6, 7)));
 
@@ -110,6 +110,7 @@ public class TestChessGameComponents {
     public void testHorsePossibleMoves() {
         Piece horse = new Horse();
         List<Coordinate> coordinates = horse.possibleMoves(new Coordinate("A8"));
+        //coordinates.forEach(c -> System.out.println(c.getIdentifier()));
         assertEquals(2, coordinates.size());
         assertTrue(coordinates.stream().anyMatch(v -> v.getIdentifier().equals("B6")));
         assertTrue(coordinates.stream().anyMatch(v -> v.getIdentifier().equals("C7")));
@@ -130,6 +131,7 @@ public class TestChessGameComponents {
     public void testKingPossibleMoves() {
         Piece king = new King();
         List<Coordinate> coordinates = king.possibleMoves(new Coordinate("D5"));
+        //coordinates.forEach(c -> System.out.println(c.getIdentifier()));
         assertEquals(8, coordinates.size());
         assertTrue(coordinates.stream().anyMatch(v -> v.getIdentifier().equals("C6")));
         assertTrue(coordinates.stream().anyMatch(v -> v.getIdentifier().equals("C5")));
@@ -145,6 +147,7 @@ public class TestChessGameComponents {
     public void testQueenPossibleMoves() {
         Piece queen = new Queen();
         List<Coordinate> coordinates = queen.possibleMoves(new Coordinate("D5"));
+        //coordinates.forEach(c -> System.out.println(c.getIdentifier()));
         assertEquals(63, coordinates.size());
         assertFalse(coordinates.stream().anyMatch(v -> v.getIdentifier().equals("D5")));
         assertTrue(coordinates.stream().anyMatch(v -> v.getIdentifier().equals("A1")));
@@ -161,7 +164,7 @@ public class TestChessGameComponents {
     public void testBishop() {
         Piece bishop = new Bishop();
         List<Coordinate> coordinates = bishop.possibleMoves(new Coordinate("D5"));
-        coordinates.forEach(c -> System.out.println(c.getIdentifier()));
+        //coordinates.forEach(c -> System.out.println(c.getIdentifier()));
         assertEquals(13, coordinates.size());
 
 
