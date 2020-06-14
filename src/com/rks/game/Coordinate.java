7b80da12873415a6f94807f3e53686b9ffe28d34
Rakesh Kumar Suthar <rksuthar19@gmail.com>
2020-06-14 23:47:50 +0530

    Adding coordinate to represent the box position

---
+++ src/com/rks/game/Coordinate.java
@@ -0,0 +1,31 @@
+package com.rks.game;
+
+class Coordinate {
+    private int x_pos;
+    private int y_pos;
+
+    Coordinate(int x_pos, int y_pos) {
+        this.x_pos = x_pos;
+        this.y_pos = y_pos;
+    }
+    
+    public int getX_pos() {
+        return x_pos;
+    }
+
+    public int getY_pos() {
+        return y_pos;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "x_pos=" + x_pos +
+                ", y_pos=" + y_pos +
+                '}';
+    }
+
+    public String getIdentifier() {
+        return (char) ('A' + getX_pos()) + "" + (char) ('1' + getY_pos());
+    }
+}
--- tests/com/rks/game/TestPiece.java
+++ tests/com/rks/game/TestPiece.java
@@ -11,4 +11,25 @@ public class TestPiece {
         Piece horse = new Horse();
         assertEquals("Horse", horse.getName());
     }
+
+    @Test
+    public void testCoordinates() {
+        Coordinate coordinate;
+
+        coordinate = new Coordinate(0, 0);
+        assertEquals("A1", coordinate.getIdentifier());
+        System.out.println(coordinate);
+
+        coordinate = new Coordinate(0, 1);
+        assertEquals("A2", coordinate.getIdentifier());
+        System.out.println(coordinate);
+
+        coordinate = new Coordinate(0, 7);
+        assertEquals("A8", coordinate.getIdentifier());
+        System.out.println(coordinate);
+
+        coordinate = new Coordinate(7, 7);
+        assertEquals("H8", coordinate.getIdentifier());
+        System.out.println(coordinate);
+    }
 }
